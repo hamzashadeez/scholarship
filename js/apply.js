c3b92330f0967f4488f85cafdf409e4ad6e97031
hamzashadeez <hsaulawaj@shakeupcocktails.com>
2023-02-06 20:39:44 +0100

    few updates

--- js/apply.js
+++ js/apply.js
@@ -6,10 +6,12 @@ const state = document.getElementById("state");
 const phone = document.getElementById("phone");
 const address = document.getElementById("address");
 const nin = document.getElementById("nin");
+const noOfWifes = document.getElementById("noOfWifes");
+const noOfChildren = document.getElementById("noOfChildren");
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  let data = {
+  let newData = {
     fullname: fullname.value,
     email: email.value,
     school: school.value,
@@ -18,10 +20,27 @@ document.getElementById("form").addEventListener("submit", (e) => {
     phone: phone.value,
     address: address.value,
     nin: nin.value,
+    noOfWifes: noOfWifes.value,
+    noOfChildren: noOfChildren.value,
+    status: "Pending",
   };
-
-  let newData = [...JSON.parse(localStorage.getItem("@data")), data];
-  localStorage.setItem("@data", JSON.stringify(newData));
+  fetch(`https://mee-backend.vercel.app/dyda`, {
+    // fetch(`http://localhost:5000/dyda`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert("Application Successful ✅, We will get back to you soon!");
+    })
+    .catch((error) => {
+      console.error("catch: ", error);
+    });
+  // let newData = [...JSON.parse(localStorage.getItem("@data")), data];
+  // localStorage.setItem("@data", JSON.stringify(newData));
   // window.location.href = "";
-  alert("Application Successful ✅, We will get back to you soon!");
 });
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,51 @@
+const url = "https://mee-backend.vercel.app/dyda";
+let DATA = [];
+
+const getdata = async () => {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      render(data);
+      stat(data);
+      DATA = data;
+    })
+    .catch((error) => console.log(error));
+};
+
+window.onload = () => {
+  console.log("yee");
+  getdata();
+};
+
 const table = document.getElementById("table-body");
 const data = JSON.parse(localStorage.getItem("@data"));
 console.log(data);
 
-const render = () => {
+const render = (data) => {
   table.innerHTML = "";
   data.map((d) => {
+    console.log("dddd: ", d);
     let html = `<tr><td>${d.fullname}</td><td>${d.email}</td><td>${d.certificate}</td></tr>`;
     table.innerHTML += html;
   });
 };
 
-render();
+const stat = (data) => {
+  let rejected = 0;
+  let approved = 0;
+  let pending = 0;
+
+  data.map((d) => {
+    if (d.status === "Pending") {
+      pending++;
+    } else if (d.status === "Approved") {
+      approved++;
+    } else {
+      rejected++;
+    }
+  });
+  document.getElementById("pending").innerHTML = pending;
+  document.getElementById("approved").innerHTML = approved;
+  document.getElementById("rejected").innerHTML = rejected;
+};
